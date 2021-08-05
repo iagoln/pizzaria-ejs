ea5a9471fb00ec5d6dfc93b97a433986daf003f2
Melissa Amorim <melissa18.mo@gmail.com>
2021-08-05 20:49:14 -0300

    adicionado rota-2

--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -26,6 +26,6 @@ router.get('/pizzas/create', PizzasController.create);
 router.post('/pizzas/create',uploadDeFotoDePizza.single('img'), PizzasController.store);
 router.get('/pizzas/:id', PizzasController.show);
 router.get('/busca', PizzasController.busca);
-router.get('/melissa',PizzasController.index);
+router.get('/melissa',PizzasMelissa.index);
 
 module.exports = router;
