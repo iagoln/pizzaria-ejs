36e485b437e2360c32c374e3cf772e0dcfe34f3e
Romsgs <rsalobrena@gmail.com>
2021-08-05 20:25:41 -0300

    correção do nome da pizzaria

--- controllers/pizzasRomulo.js
+++ controllers/pizzasRomulo.js
@@ -1,5 +1,5 @@
 module.exports = {
     index: (req, res) => {
-        res.render('pizza-create2', { nome:'pizzaria di Romulo' })
+        res.render('pizza-create2', { nome:'Pizzaria Di Pietro' })
     }
 }
