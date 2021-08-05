12b05678640883ccf05c1c4548a58ed90e71324c
Leandro Coelho <lecoelhoofficial@gmail.com>
2021-08-05 20:15:34 -0300

    Adicionado Controller para renderizar pizza-create2

--- controllers/PizzasLeandroCoelho.js
+++ controllers/PizzasLeandroCoelho.js
@@ -1,1 +1,10 @@
 //teste Leandro Coelho
+const pizzas = require('../database/Pizzas.json');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+	index: (req, res)=>{
+		res.render("pizza-create2", {pizzas});
+	},
+}
